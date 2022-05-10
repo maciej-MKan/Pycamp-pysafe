8e30e46f04a2ed1de2945f17e13dae2738fa9533
Maciej <kanabaj.maciej@gmail.com>
2022-05-10 21:54:45 +0200

    Added header to encoded file

--- file_coder.py
+++ file_coder.py
@@ -5,11 +5,25 @@ class FileCoder:
         self.passwd = passwd or 'default'
 
     def encode_file(self, file_path):
+        _ , extension = path.splitext(file_path)
         new_path = self._generate_file_name(file_path)
+        content = extension + '<>' + self.passwd + '<>'
+        content = content.encode('utf-8')
+        #print(new_path)
         try:
             rename(file_path, new_path)
         except PermissionError as err:
             print(err)
+        #print(content)
+        else:
+            with open(new_path, 'rb') as file:
+                file_content = file.read()
+                #print(file_content)
+            with open(new_path, 'wb') as file:
+                content = content + file_content
+                #print(content)
+                file.write(content)
+
     
     def _generate_file_name(self, cur_path : str, count : int = 0):
         new_path = cur_path.split('.')
