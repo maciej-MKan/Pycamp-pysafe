adf486cd4b1526ffa160c6dd9e648fe0143f621a
Maciej <kanabaj.maciej@gmail.com>
2022-05-11 22:41:13 +0200

    modified decoder to context manager, added file restore when decoding failed, added reference to the encoder

--- file_coder.py
+++ file_coder.py
@@ -1,29 +1,35 @@
 from os import rename, path
+from time import sleep
 
 class FileCoder:
     def __init__(self, passwd = None) -> None:
         self.passwd = passwd or 'default'
 
-    def encode_file(self, file_path):
-        _ , extension = path.splitext(file_path)
-        new_path = self._generate_file_name(file_path)
-        content = extension + '<>' + self.passwd + '<>'
-        content = content.encode('utf-8')
-        #print(new_path)
-        try:
-            rename(file_path, new_path)
-        except PermissionError as err:
-            print(err)
-        #print(content)
+    def encode_file(self, file_path, retrys = 0):
+        if retrys < 60:
+            _ , extension = path.splitext(file_path)
+            new_path = self._generate_file_name(file_path)
+            content = extension + '<>' + self.passwd + '<>'
+            content = content.encode('utf-8')
+            #print(new_path)
+            try:
+                rename(file_path, new_path)
+            except PermissionError as err:
+                print(err)
+                sleep(1)
+                retrys += 1
+                self.encode_file(file_path, retrys)
+            #print(content)
+            else:
+                with open(new_path, 'rb') as file:
+                    file_content = file.read()
+                    #print(file_content)
+                with open(new_path, 'wb') as file:
+                    content = content + file_content
+                    #print(content)
+                    file.write(content)
         else:
-            with open(new_path, 'rb') as file:
-                file_content = file.read()
-                #print(file_content)
-            with open(new_path, 'wb') as file:
-                content = content + file_content
-                #print(content)
-                file.write(content)
-
+            raise TimeoutError('no access to file')
     
     def _generate_file_name(self, cur_path : str, count : int = 0):
         new_path = cur_path.split('.')
--- file_decoder.py
+++ file_decoder.py
@@ -1,20 +1,38 @@
 from os import walk, rename, path
+from tempfile import TemporaryFile
+from sys import argv
 
 class FileDecoder:
 
     def __init__(self, passwd = None) -> None:
         self.passwd = passwd or 'default'
+        self.file_path = None
+        self.tmp = TemporaryFile()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            self.restore_file()
+        self.tmp.close()
 
     def decode_file(self, file_path):
+        self.file_path = file_path
         if not path.isfile(file_path):
             raise FileNotFoundError
         
-        with open(file_path, 'rb') as file:
+        with open(file_path, 'r+b') as file:
             file_content = file.read()
-            file_extension, passwd, content = file_content.split(b'<>')
+            #file.seek(0)
 
-        with open(file_path, 'wb') as file:
-            file.write(content)
+            self.tmp.write(file_content)
+            self.tmp.seek(0)
+
+            file_extension, passwd, *content = file_content.split(b'<>')
+
+        with open(file_path, 'w+b') as file:
+            file.write(b'<>'.join(content))
 
         new_path, _ = path.splitext(file_path)
         new_path += file_extension.decode('utf-8')
@@ -38,6 +56,25 @@ class FileDecoder:
             if file_extension.startswith('.pys'):
                 self.decode_file(file_path)
 
+    def restore_file(self):
+        with open(self.file_path, 'wb') as file:
+            tmp_content = self.tmp.read()
+            file.write(tmp_content)
+
+    def __del__(self):
+        self.tmp.close()
+
 if __name__ == '__main__':
-    my_decoder = FileDecoder()
-    my_decoder.decode_all(r'.\Nowy_Folder')
+    # my_decoder = FileDecoder()
+    # my_decoder.decode_all(r'.\Nowy_Folder')
+
+    if len(argv) == 2:
+        if path.isfile(argv[1]):
+            with FileDecoder() as decoder:
+                decoder.decode_file(argv[1])
+        elif path.isdir(argv[1]):
+            with FileDecoder() as decoder:
+                decoder.decode_all(argv[1])
+    else:
+        with FileDecoder() as decoder:
+            decoder.decode_all(r'.\Nowy_Folder')
